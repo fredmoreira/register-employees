bc31685b00aa8dbd4e9e2705500e5e543d7027fb
Frederico Moreira <fredmoreira@Fredericos-MBP.lan>
2019-05-01 16:04:21 +0100

    Fixes on routes file and package.json

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,10 @@ router.get('/', (req, res) => {
 });
 
 router.get('/employees', (req, res) => {
-  return res.send('Received a GET HTTP method');
+  let params = (req.query ? req.query : {});
+  if (params.name === '') {
+    return res.sendStatus(204);
+  }
 });
 
 router.post('/employees', (req, res) => {
