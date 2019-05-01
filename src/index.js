d615eec869c7495e53410167df3510be6290b2d7
Frederico Moreira <fredmoreira@Fredericos-MBP.lan>
2019-05-01 22:13:00 +0100

    Correct index file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const routes = require('./routes');
+import express from 'express';
+import routes from './routes/employee-routes';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose'
+
 
 const app = express();
 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/employees', {
+  useNewUrlParser: true
+});
+
 app.set('json spaces', 2);
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.use(bodyParser.json());
-app.use('/', routes);
+routes(app);
 
 app.set('port', process.env.PORT || 5000);
 
@@ -18,4 +28,4 @@ const server = app.listen(app.get('port'), function(err) {
     .port);
 });
 
-module.exports = app;
+export default app;
