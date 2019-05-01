67b284f52590b5e71d8284da64fde048b71b550d
Frederico Moreira <fredmoreira@Fredericos-MBP.lan>
2019-05-01 23:50:22 +0100

    Implemented validations

--- src/controllers/employee-controller.js
+++ src/controllers/employee-controller.js
@@ -4,19 +4,29 @@ import employeeSchema from '../models/employees-model'
 const Employee = mongoose.model('EmployeeSchema', employeeSchema)
 
 export function addNewEmployee(req, res) {
+
   let newEmployee = new Employee(req.body)
   newEmployee.save((error, employee) => {
     if (error) {
-      res.json(400)
+      res.status(400).json(error)
     }
     res.json(employee)
   })
 }
 
 export function getEmployees(req, res) {
-  Employee.find({}, (error, employees) => {
+  let params = (req.query ? req.query : {});
+
+  if (params.name === '') {
+    return res.sendStatus(400);
+  }
+
+  Employee.find(params, (error, employees) => {
     if (error) {
       res.json(error)
+    }
+    if (!employees.length) {
+      return res.sendStatus(404);
     }
     res.json(employees)
   })
@@ -26,6 +36,9 @@ export function getEmployee(req, res) {
   Employee.findById(req.params.id, (error, employee) => {
     if (error) {
       res.json(error)
+    }
+    if (!employee.length) {
+      return res.sendStatus(404);
     }
     res.json(employee)
   })
