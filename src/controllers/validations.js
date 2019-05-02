6d857be3938fded48c7976d5a6cf3e592ef279a9
Frederico  Moreira <frederico.moreira@farfetch.com>
2019-05-02 17:59:12 +0100

    Implemented validation schema

--- src/controllers/validations.js
+++ src/controllers/validations.js
@@ -1,14 +1,15 @@
 import Joi from '@hapi/joi';
 
 const schema = Joi.object().keys({
-    name: Joi.string().alphanum().min(3).max(30).required(),
-    nif: Joi.number().regex(/^[a-zA-Z0-9]{3,30}$/),
-    access_token: [Joi.string(), Joi.number()]
+    name: Joi.string().required(),
+    nif: Joi.number().required(),
+    address: Joi.string()
 });
 
 // Return result.
-const result = Joi.validate({ username: 'abc', birthyear: 1994 }, schema);
+const result = Joi.validate({ name: 'abc', nif: 999555888 }, schema);
+console.log(result);
 // result.error === null -> valid
 
 // You can also pass a callback which will be called synchronously with the validation result.
-Joi.validate({ username: 'abc', birthyear: 1994 }, schema, function (err, value) { });
+Joi.validate({ username: 'abc', nif: 999555444 }, schema, function (err, value) { });
