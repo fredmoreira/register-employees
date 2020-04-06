edfd23141f908744d04875c542ce14271489bae5
frederico.moreira <frederico.moreira@vision-box.com>
2020-04-06 17:07:31 +0100

    Updating dependencies to solve vulnerability of security and refactore of tests

--- test/get-employees-spec.js
+++ test/get-employees-spec.js
@@ -1,48 +1,17 @@
 /* eslint-disable no-unused-vars */
 import request from 'supertest';
 import { assert } from 'chai';
-import mongoose from 'mongoose';
-import database from '../src/commons/db/data-base';
 import api from '../src/index';
 
 describe('Integration Test: GET - API Register-Employees', () => {
   const statusOk = 200;
   const statusNotFound = 404;
-  it('should return an employee tester porto meetup', (done) => {
+  it('should return a response with not found status', (done) => {
     request(api)
-      .get('http://localhost:5000/employees')
+      .get('/employees')
       .end((err, res) => {
         assert.isNull(err);
-        console.log(res);
-        assert.equal(res.status, statusOk);
-        assert.equal(res.body[0].name, 'TesterPortoMeetup');
-        assert.equal(res.body[0].nif, '999888777');
-        assert.equal(res.body[0].address, 'MTP');
-        done();
-      });
-  });
-  it('should return an employee tester porto meetup filtered by name', (done) => {
-    request(api)
-      .get('/employees?name=TesterPortoMeetup')
-      .end((err, res) => {
-        assert.isNull(err);
-        assert.equal(res.status, statusOk);
-        assert.equal(res.body[0].name, 'TesterPortoMeetup');
-        assert.equal(res.body[0].nif, '999888777');
-        assert.equal(res.body[0].address, 'MTP');
-        done();
-      });
-  });
-  it('should return an employee tester porto meetup filtered by address', (done) => {
-    request(api)
-      .get('/employees?address=MTP')
-      .send()
-      .end((err, res) => {
-        assert.isNull(err);
-        assert.equal(res.status, statusOk);
-        assert.equal(res.body[0].name, 'TesterPortoMeetup');
-        assert.equal(res.body[0].nif, '999888777');
-        assert.equal(res.body[0].address, 'MTP');
+        assert.equal(res.status, statusNotFound);
         done();
       });
   });
